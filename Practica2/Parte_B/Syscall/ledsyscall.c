6789c633ab6a147679660a4363ef2fd5ba88ec0d
markdelapuerta <marccu01@ucm.es>
2015-11-11 13:15:38 +0100

    update

--- Practica2/Parte_B/Syscall/ledsyscall.c
+++ Practica2/Parte_B/Syscall/ledsyscall.c
@@ -4,6 +4,9 @@
 #include <linux/tty.h>      /* For fg_console */
 #include <linux/kd.h>       /* For KDSETLED */
 #include <linux/vt_kern.h>
+#include <asm-generic/errno-base.h>
+#include <linux/errno.h>
+
 
 struct tty_driver* kbd_driver= NULL;  /* Driver of leds */
 
@@ -20,24 +23,34 @@ static inline int set_leds(struct tty_driver* handler, unsigned int mask){
 }
 
 /* Set input num to correctly mask */
-int check(int num) {
-	int ret = num;
-	switch(num) {
-		case 2: ret = 4; break;
-		case 3: ret = 5; break;
-		case 4: ret = 2; break;
-		case 5: ret = 3; break;
+int convert(int num) {
+	int i;
+	int status = 0;	
+	for(i=0;i<3;i++){
+	  if(num&(0x1 << i)) {
+	  	switch(i) {
+			case 0: status |=1;
+			case 1: status |=4;
+			case 2: status |=2;
+		}
+		printf("Status ==> %d\n",status);
+	  }	
 	}
-	return ret;
+	return status;
 }
 
 /* This syscall modify the the led state */
 SYSCALL_DEFINE1(ledctl,unsigned int,leds) {
 	if((leds<0)||(leds>7))
-		return -1;
+		return -EINVAL;
 	kbd_driver= get_kbd_driver_handler();
-	leds = check(leds);
-	set_leds(kbd_driver,leds);
+
+	if(kbd_driver==NULL)
+		return -ENODEV;
+
+	leds = convert(leds);
+	if(set_leds(kbd_driver,leds) < 0)
+		return -ENOTSUPP;
 	return 0;
 }
 
